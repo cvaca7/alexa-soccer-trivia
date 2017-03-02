1d3631e843c6a75388e778fbe1e9fce1a8f50a88
Carlos A. Vaca Morales <carlosv@rain.agency>
2017-03-02 09:40:28 -0600

    ES2015 on app.js

--- src/app.js
+++ src/app.js
@@ -2,17 +2,34 @@ var app = require('express')();
 var bodyParser = require('body-parser');
 var alexaSkill = require('./skill');
 
-app.use(bodyParser.urlencoded( { extended: false } ));
-app.use(bodyParser.json());
-
-
+app
+    .use(bodyParser.urlencoded( { extended: false } ));
+    .use(bodyParser.json());
+    .post('/', (request,response,next)  => {
+        alexaSkill.handler( request.body,  {
+            fail: next,
+            succeed: (msg) => {
+            response.json(msg);
+            }
+        },  (error,msg) => {
+            if(error) return next(err);
+            return response.json(msg);
+        } );
+    })
+    .get('/', (request,response) => {
+        response.send('<h1>Alexa Skill Application</h1>');
+    })
+    .listen(3000, (request,response) => {
+        console.log('App listening on port 3000');
+    });
+/*
 app.post('/', function(request,response, next) {
 
-	alexaSkill.handler(request.body, { 
-		fail: function(error){ next(error); }, 
-		succeed: function(result){ response.json(result); } }, 
-		function(err,msg) { if(err) return next(err); return response.json(msg); }
-	);
+    alexaSkill.handler(request.body, {
+            fail: function(error){ next(error); },
+            succeed: function(result){ response.json(result); } },
+        function(err,msg) { if(err) return next(err); return response.json(msg); }
+    );
 });
 
 app.get('/', function(request,response){
@@ -22,3 +39,4 @@ app.get('/', function(request,response){
 app.listen(3000, function(request,response){
 	console.log('app listening on port 3000');
 });
+    */
